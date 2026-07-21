6cb1893cf6d15a016aebc168961d8ce409fd9278
yefenglei <1207717096@qq.com>
2026-07-21 16:05:42 +0800

    初始化项目

--- config/config.py
+++ config/config.py
@@ -5,16 +5,64 @@
 @Author  : @叶风磊
 @Desc    : 配置信息
 """
-#环境基准地址：
-BASE_URL = 'http://127.0.0.1:8888/api/private/v1'
 
-#excel格式的测试用例文件配置
-EXCEL_FILE = './data/测试用例.xlsx'
-SHEET_NAME = 'Sheet1'
+import os
 
-#mysql 配置信息
-DB_HOST = '127.0.0.1'
-DB_USER = 'root'
-DB_PASSWORD = '123456'
-DB_NAME = 'mydb'
-DB_PORT = 3306
+# 默认环境
+ENV = os.getenv("TEST_ENV", "test")
+
+# 多环境配置
+config = {
+    "dev": {
+        #环境基准地址：
+        "BASE_URL" : 'http://127.0.0.1:8888/api/private/v1',
+        #excel格式的测试用例文件配置
+        "EXCEL_FILE" :'./data/测试用例.xlsx',
+        "SHEET_NAME" : 'Sheet1',
+        #mysql 配置信息,
+        "DB_HOST" : '127.0.0.1',
+        "DB_USER" : 'root',
+        "DB_PASSWORD" : '123456',
+        "DB_NAME" : 'mydb',
+        "DB_PORT" : 3306
+    },
+    "test": {
+        #环境基准地址：
+        "BASE_URL" : 'http://127.0.0.1:8888/api/private/v1',
+        #excel格式的测试用例文件配置
+        "EXCEL_FILE" :'./data/测试用例.xlsx',
+        "SHEET_NAME" : 'Sheet1',
+        #mysql 配置信息,
+        "DB_HOST" : '127.0.0.1',
+        "DB_USER" : 'root',
+        "DB_PASSWORD" : '123456',
+        "DB_NAME" : 'mydb',
+        "DB_PORT" : 3306
+    },
+    "pre": {
+        #环境基准地址：
+        "BASE_URL" : 'http://127.0.0.1:8888/api/private/v1',
+        #excel格式的测试用例文件配置
+        "EXCEL_FILE" :'./data/测试用例.xlsx',
+        "SHEET_NAME" : 'Sheet1',
+        #mysql 配置信息,
+        "DB_HOST" : '127.0.0.1',
+        "DB_USER" : 'root',
+        "DB_PASSWORD" : '123456',
+        "DB_NAME" : 'mydb',
+        "DB_PORT" : 3306
+    },
+    "prod": {
+        #环境基准地址：
+        "BASE_URL" : 'http://127.0.0.1:8888/api/private/v1',
+        #excel格式的测试用例文件配置
+        "EXCEL_FILE" :'./data/测试用例.xlsx',
+        "SHEET_NAME" : 'Sheet1',
+        #mysql 配置信息,
+        "DB_HOST" : '127.0.0.1',
+        "DB_USER" : 'root',
+        "DB_PASSWORD" : '123456',
+        "DB_NAME" : 'mydb',
+        "DB_PORT" : 3306
+    }
+}
